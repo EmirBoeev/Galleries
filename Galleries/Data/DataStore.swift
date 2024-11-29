bf938bf6f3dbc919db41c5c8a268a0fafa177bcc
Emir Boeev <emir.boeev@iMacK.local>
2024-11-29 13:00:46 +0100

    ff

---
+++ Galleries/Data/DataStore.swift
@@ -0,0 +1,19 @@
+//
+//  DataStore.swift
+//  Galleries
+//
+//  Created by Emir Boeev on 29/11/2024.
+//
+
+import Foundation
+
+@Observable
+class DataStore {
+    var galleries : [Gallery]
+    var selectedGallery : Gallery?
+    
+    init() {
+        
+    }
+    
+}
